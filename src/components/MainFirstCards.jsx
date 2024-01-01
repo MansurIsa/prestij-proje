cd53cd2e1880e83f12a229471c3990a129c176aa
MansurIsa <mansur69722@gmail.com>
2024-01-01 21:49:54 +0400

    updated

--- src/components/MainFirstCards.jsx
+++ src/components/MainFirstCards.jsx
@@ -2,19 +2,31 @@ import React from 'react'
 import OnlineExamIcon from '../images/online-course (1).png'
 import OnlineSignUpIcon from '../images/registered (1).png'
 import OnlineExamResultIcon from '../images/checklist (4).png'
+import { useNavigate } from 'react-router-dom'
 
 const MainFirstCards = () => {
+
+    const navigate=useNavigate()
+
+    const courseOnlineRegister=()=>{
+        navigate('/course-online-register')
+    }
+
+    const onlineExamResults=()=>{
+        navigate('/imtahan-neticeleri')
+    }
+
     return (
         <div className="main_first_cards">
             <ul className='main_second_cards_container container'>
-                <li className="header_second_card">
+                <li onClick={onlineExamResults} className="header_second_card">
                     <div>
                         <img src={OnlineExamResultIcon} alt="" />
                         
                     </div>
                     <p>İmtahan nəticələri</p>
                 </li>
-                <li className="header_second_card">
+                <li onClick={courseOnlineRegister} className="header_second_card">
                     <div>
                         <img src={OnlineSignUpIcon} alt="" />
                     </div>
