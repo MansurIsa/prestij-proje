7ec3e543d4a1b97f870faf889aa376daabad921d
MansurIsa <mansur69722@gmail.com>
2024-05-12 05:23:16 +0400

    updated

--- src/components/OtherHeaderContent.jsx
+++ src/components/OtherHeaderContent.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const OtherHeaderContent = ({ otherHeaderMainContent }) => {
   return (
-    <div style={{ width: "100%"}} className="container">
+    <div style={{ width: "100%"}} className="container other_header_content">
       <h1>{otherHeaderMainContent}</h1>
     </div>
 
